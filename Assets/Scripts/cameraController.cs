56fd35beca0fcd468de434231f4a3be6e75beb47
CeilingFan43 <xxeonduckxx@gmail.com>
2025-03-24 04:00:27 +0800

    Creating camera controller script

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public Transform playerOrientation;
+    public Transform player;
+    public Transform playerObject;
+    public Rigidbody rb;
+    
+    public float rotationSpeed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Getting the player direction vector
+        Vector3 viewDirection = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+
+        // Setting the player direction
+        playerOrientation.forward = viewDirection.normalized;
+
+        // Rotating the player
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 inputDirection = playerOrientation.forward * verticalInput + playerOrientation.right * horizontalInput;
+
+        if(inputDirection != Vector3.zero)
+        {
+            playerObject.forward = Vector3.Slerp(playerObject.forward, inputDirection.normalized, Time.deltaTime * rotationSpeed);
+        }
+        
+    }
+}
